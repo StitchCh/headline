f4555fc54865891aa06b4113d1eed5b0273f64f7
zhuxianyong <zhuxianyong@eversec.cn>
2018-06-07 18:29:58 +0800

    ...

--- src/apps.1/404/router.js
+++
@@ -1,6 +0,0 @@
-export default {
-  path: '*',
-  name: '404',
-  component: resolve => require(['@/apps/404/index.vue'], resolve),
-  meta: { title: 'CCTP' }
-}
--- src/apps.1/medialibrary/router.js
+++
@@ -1,6 +0,0 @@
-export default {
-  path: '/media',
-  name: 'Medialibrary',
-  component: resolve => require(['@/apps/medialibrary/index.vue'], resolve),
-  meta: { level: 2, title: '媒体库', identifier: 'medialibrary' }
-}
