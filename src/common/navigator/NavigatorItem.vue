8c8d923c5d5adc5d457c9dcc0af262e9e0ebc219
changhaonan <changhaonan@eversec.com>
2018-06-07 15:22:52 +0800

    侧边导航初步完成

---
+++ src/common/navigator/NavigatorItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="navigator-item relative" @click="$emit('click')">
+    <i class="icon" v-if="icon">{{icon}}</i>
+    <span :class="[ 'status',  status ]" v-if="status"></span>
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'navigator-item',
+    props: {
+      icon: String,
+      status: String
+    },
+    created () {
+      console.log(this.$parent)
+    }
+  }
+</script>
+
+<style lang="less">
+.navigator-item {height: 45px;border-bottom: 1px solid rgba(0, 0, 0, .05);box-sizing: border-box;padding: 10px 20px;cursor: pointer;
+  .icon {font-size: 20px;margin-right: 5px;}
+  .status {display: inline-block;width: 10px;height: 10px;border-radius: 5px;margin-right: 5px;}
+  .done {background: #4ab36b;}
+  .warning {background: #f89972;}
+  .error {background: #d74947;}
+  .disabled {background: #b8a59a;}
+}
+</style>
--- src/common/navigator/index.js
+++ src/common/navigator/index.js
@@ -1,7 +1,11 @@
-import Navigator from './Navigator.vue'
+import Navigator from './Navigator'
+import NavigatorItem from './NavigatorItem'
+import NavigatorItemGroup from './NavigatorItemGroup'
 
 Navigator.install = function (Vue) {
   Vue.component(Navigator.name, Navigator)
+  Vue.component(NavigatorItem.name, NavigatorItem)
+  Vue.component(NavigatorItemGroup.name, NavigatorItemGroup)
 }
 
-export default Navigator
+export default { Navigator, NavigatorItem, NavigatorItemGroup }
