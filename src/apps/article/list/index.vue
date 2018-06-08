6bb7bd26070e99748562bcc871903c3e89e08296
zhuxianyong <zhuxianyong@eversec.cn>
2018-06-08 18:46:24 +0800

    列表数据绑定

--- src/apps/article/list/index.vue
+++ src/apps/article/list/index.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="flex flex-item">
     <af-center @add="$router.push('/articleAdd')" url="/cri-cms-platform/article/list.monitor">
-    <div class="list-item a" slot-scope="slotProps">
-      <div class="list-title flex-v-center">
-        <i class="icon f-16 blue">thumb_up</i>
-        <i class="icon f-16 orange">image</i>
-        <span class="flex-item c-6 f-14 b">俄加里宁格勒今年将向中国出口360吨琥珀俄加里宁格勒今年将向中国出口360吨琥珀</span>
+      <div class="list-item a" slot-scope="slotProps">
+        <div class="list-title flex-v-center">
+          <i v-if="~~(slotProps.item.isRecommnd)" class="icon f-16 blue">thumb_up</i>
+          <i v-if="~~(slotProps.item.hasThumb)" class="icon f-16 orange">image</i>
+          <i v-if="~~(slotProps.item.isOriginal)" class="icon f-16 green">copyright</i>
+          <span class="flex-item c-6 f-14 b">{{slotProps.item.title}}</span>
+        </div>
+        <div class="list-info f-12 c-8 flex-v-center">
+          <span>{{slotProps.item.createDate}}</span>
+          <span>{{slotProps.item.createUser}}</span>
+          <span class="list-info-num">
+            <i v-tooltip:top="'阅读'">6</i>/<i v-tooltip:top="'评论'">0</i>/<i v-tooltip:top="'分享'">0</i>
+          </span>
+          <span class="flex-item"></span>
+          <i class="icon f-14 c-a">computer</i>
+          <i class="icon f-14 c-a">phone_iphone</i>
+          <i class="icon f-14 c-a">public</i>
+        </div>
       </div>
-      <div class="list-info f-12 c-8 flex-v-center">
-        <span>2018-09-09</span>
-        <span>超级管理员</span>
-        <span class="list-info-num">
-          <i v-tooltip:top="'阅读'">6</i>/<i v-tooltip:top="'评论'">0</i>/<i v-tooltip:top="'分享'">0</i>
-        </span>
-        <span class="flex-item"></span>
-        <i class="icon f-14 c-a">computer</i>
-        <i class="icon f-14 c-a">phone_iphone</i>
-        <i class="icon f-14 c-a">public</i>
-      </div>
-    </div>
     </af-center>
     <div class="flex-item flex-col">
       <div class="af-topbar flex-v-center">
