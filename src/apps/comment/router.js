a301e7e66aaceb44a4d3edd5069c741f3ca1b406
changhaonan <changhaonan@eversec.cn>
2018-12-24 01:30:43 +0800

    评论模块

--- src/apps/comment/router.js
+++ src/apps/comment/router.js
@@ -2,9 +2,15 @@ export default {
   path: '/comment',
   component: resolve => require(['@/apps/comment/index.vue'], resolve),
   meta: { level: 2, title: '评论', identifier: 'comment' },
-  children: [{
-    path: '/comment/list',
-    name: 'CommentList',
-    component: resolve => require(['@/apps/comment/list/index.vue'], resolve)
-  }]
+  children: [
+    {
+      path: '/',
+      redirect: '/comment/list'
+    },
+    {
+      path: 'list',
+      name: 'CommentList',
+      component: resolve => require(['@/apps/comment/list/index.vue'], resolve)
+    }
+  ]
 }
