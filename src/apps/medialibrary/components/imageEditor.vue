4480c90444016c78d6f4a3ef5ad2842226e0ce4c
wutong <wutong1@ultrapower.com.cn>
2018-12-31 14:19:09 +0800

    Bug 修复

--- src/apps/medialibrary/components/imageEditor.vue
+++ src/apps/medialibrary/components/imageEditor.vue
@@ -313,7 +313,31 @@ export default {
               }
             }
             this.mycanvas.drawImage(imgObj, (this.watermarkData.left - (this.imgboxData.width - canvasData.ratioValue.w) / 2) / canvasData.ratio, (this.watermarkData.top - (this.imgboxData.height - canvasData.ratioValue.h) / 2) / canvasData.ratio, this.watermarkData.width / canvasData.ratio, this.watermarkData.width / canvasData.ratio)
-            let img = canvas.toDataURL("image/png")
+            var convertBase64ToBlob = function(base64){
+              var base64Arr = base64.split(',');
+              var imgtype = '';
+              var base64String = '';
+              if(base64Arr.length > 1){
+                //如果是图片base64，去掉头信息
+                base64String = base64Arr[1];
+                imgtype = base64Arr[0].substring(base64Arr[0].indexOf(':')+1,base64Arr[0].indexOf(';'));
+              }
+              // 将base64解码
+              var bytes = atob(base64String);
+              //var bytes = base64;
+              var bytesCode = new ArrayBuffer(bytes.length);
+              // 转换为类型化数组
+              var byteArray = new Uint8Array(bytesCode);
+
+              // 将base64转换为ascii码
+              for (var i = 0; i < bytes.length; i++) {
+                byteArray[i] = bytes.charCodeAt(i);
+              }
+
+              // 生成Blob对象（文件对象）
+              return new Blob( [bytesCode] , {type : imgtype});
+            }
+            let img = convertBase64ToBlob(canvas.toDataURL("image/png"))
             let data = {
               type: 0,
               folderId: current.folderId,
