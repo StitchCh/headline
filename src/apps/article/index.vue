5a1ae8a74f53c473146b779bace650451a287fe1
ashou <5304405+ashouAAA@user.noreply.gitee.com>
2020-07-02 11:48:17 +0800

    正式服  文章专题权限修改

--- src/apps/article/index.vue
+++ src/apps/article/index.vue
@@ -4,7 +4,7 @@
     <navigator-item icon="folder" index="all" @click="() => {if ($route.query.status != 'all') {$router.replace('/article/list?status=all')}}">全部</navigator-item>
     <!-- <navigator-item-group defaultExtended index="2" icon="face">
     <span slot="title">我的</span> -->
-    <navigator-item icon="folder" index="article_theme" @click="$router.replace('/article/list?article_theme=true&status=article_theme')">专题</navigator-item>
+    <navigator-item v-if="roleName == 'SUPER_ADMIN'" icon="folder" index="article_theme" @click="$router.replace('/article/list?article_theme=true&status=article_theme')">专题</navigator-item>
     <navigator-item icon="check_circle" index="PASS" @click="() => {if ($route.query.status != 'PASS') {$router.replace('/article/list?status=PASS')}}">已审</navigator-item>
     <navigator-item icon="hourglass_full" index="AUDITING" @click="() => {if ($route.query.status != 'AUDITING') {$router.replace('/article/list?status=AUDITING')}}">待审</navigator-item>
     <navigator-item icon="error" index="ArticleReject" @click="$router.replace('/article/reject')">驳回</navigator-item>
@@ -22,6 +22,11 @@ import AfLeft from '@/components/app-frame/afLeft'
 export default {
   name: 'app-article',
   components: { AfLeft },
+  data () {
+    return {
+      roleName: ''
+    }
+  },
   computed: {
     defaultActive () {
       let { name } = this.$route
@@ -32,6 +37,9 @@ export default {
       }
       return name.replace('Content', '')
     }
+  },
+  beforeMount () {
+    this.roleName = sessionStorage.getItem('roleName')
   }
 }
 </script>
